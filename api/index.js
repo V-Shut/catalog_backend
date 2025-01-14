86047e5de39e04a5520a9da7d2a4e3e0cd4de4be
V-Shut <shut.vitaliy.ol@gmail.com>
2025-01-14 15:30:25 +0200

    add express layout

---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+
+const app = express();
+const PORT = 3000;
+
+const goods = [
+  {
+    title: "test",
+    price: 300,
+    producer: "test",
+    image: "cat.svg",
+  },
+];
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json(goods);
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+export default app;
