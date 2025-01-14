8cbe63dd41e05087d10a874d7a0a550ec9e64411
V-Shut <shut.vitaliy.ol@gmail.com>
2025-01-14 19:36:39 +0200

    add express layout

--- api/index.js
+++ api/index.js
@@ -2,16 +2,6 @@ import express from "express";
 import products from '../products.json'
 
 const app = express();
-const PORT = 3000;
-
-const products = [
-  {
-    title: "test",
-    price: 300,
-    producer: "test",
-    imageURL: "",
-  },
-];
 
 app.get("/api", (req, res) => {
   res.json(products);
