07e8b6bdb06c1cea7ab81dca9deb8f4979e70e2a
V-Shut <shut.vitaliy.ol@gmail.com>
2025-01-14 18:38:40 +0200

    add express layout

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import multer from "multer";
 import path from "path";
 
 const app = express();
@@ -14,32 +13,7 @@ const products = [
   },
 ];
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage });
-
-app.use('/uploads', express.static('uploads'));
-
-app.post('/uploads/:id', upload.single('image'), (req, res) => {
-  const {id} = req.params;
-  const imageUrl = `/uploads/${req.file.filename}`
-
-  const product = products.find(p => p.id === parseInt(id))
-
-  if (product) {
-    product.imageURL = imageUrl;
-    res.json({message: 'image added', product})
-  } else {
-    res.status(404).json({message: 'product was not found'})
-  }
-})
+app.use('/static', express.static(path.join(__dirname, 'public/img')));
 
 app.get("/api", (req, res) => {
   res.json(products);
